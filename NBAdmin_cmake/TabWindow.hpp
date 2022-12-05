12adf7fab70100baddb87b2801fbeb1899e9fd80
ziabric <kurbet12312@gmail.com>
2022-12-05 00:30:52 -0800

    minor

--- NBAdmin_cmake/TabWindow.hpp
+++ NBAdmin_cmake/TabWindow.hpp
@@ -14,6 +14,8 @@
 #include <vector>
 #include <string>
 
+#include <ctime>
+
 #include "nb-samples.h"
 #include "ResponceView.hpp"
 
@@ -37,15 +39,17 @@ public:
 public slots:
     void setCurrentIndex(int index)
     {
-        //print_result_to_table(responces_.at(index), *tableWidget_);
-//        if (currentModel_ != nullptr) delete currentModel_;
-//        currentModel_ = new ResponceView();
-        QStandardItemModel* model = new QStandardItemModel();
-        print_to_model(model, responces_.at(index));
-        tableWidget_->setModel(model);
+        std::cout<<"start set current"<<std::endl;
+        tableWidget_->setModel(models_.at(index));
+        std::cout<<"end set current"<<index<<std::endl;
     }
     void push_button_run_clicked()
     {
+        int start = clock();
+
+        std::cout<<"start click"<<std::endl;
+        for (auto item : models_) delete item;
+        models_.resize(0);
         input_queries_.clear();
         responces_.clear();
         delete comboBox_;
@@ -66,20 +70,27 @@ public slots:
                 while(tmp.at(i+1) == " " || tmp.at(i+1) == "\t" || tmp.at(i+1) == "\n") i+=1;
             }
         }
+        models_.push_back(new QStandardItemModel());
         for (int i = 0; i<int(input_queries_.size());i+=1)
         {
             NB_HANDLE connection = nb_connect( u"127.0.0.1", dbPort_, u"TESTUSER", u"1234" );
             check_error(connection);
-            responces_.push_back(exec_select_query(connection, QString::fromStdString(input_queries_.at(i))));
+            exec_select_query(connection, QString::fromStdString(input_queries_.at(i)), models_.at(i));
             nb_disconnect(connection);
             comboBox_->addItem(QString::fromStdString(input_queries_.at(i)));
         }
+        std::cout<<"end click"<<std::endl;
+
+        int end = clock();
+        int t = (end - start) / CLOCKS_PER_SEC;
+        std::cout<<t<<std::endl;
     }
 
 private:
 
     void print_to_model(QStandardItemModel* model, std::vector<std::vector<std::string> > &input)
     {
+        std::cout<<"start print"<<std::endl;
         for (int i = 0; i<input.size(); i+=1)
         {
             QList<QStandardItem*> list;
@@ -88,29 +99,13 @@ private:
                 QStandardItem* item = new QStandardItem(i,j);
                 item->setText(QString::fromStdString(input.at(i).at(j)));
                 list.append(item);
+
             }
             model->appendRow(list);
         }
+        std::cout<<"end print"<<std::endl;
     }
 
-    void print_result_to_table(std::vector<std::vector<std::string> > output, QTableWidget& tableWidget)
-    {
-        tableWidget.setColumnCount(output[0].size());
-        tableWidget.setRowCount(output.size()-1);
-        QStringList header_list;
-        for (int i = 0;i<int(output[0].size());i+=1)
-        {
-            header_list.push_back(QString::fromStdString(output[0][i]));
-        }
-        tableWidget.setHorizontalHeaderLabels(header_list);
-        for (unsigned long long i = 1;i<output.size();i+=1)
-        {
-            for (int j = 0;j<int(output[i].size());j+=1)
-            {
-                tableWidget.setItem(i-1,j, new QTableWidgetItem(QString::fromStdString(output[i][j])));
-            }
-        }
-    }
     bool check_query(NB_HANDLE connection)
     {
         if (nb_errno(connection) == NB_OK) return true;
@@ -138,10 +133,11 @@ private:
         }
         return output;
     }
-    std::vector<std::vector<std::string> > exec_select_query(NB_HANDLE connection, QString query)
+
+    void exec_select_query(NB_HANDLE connection, QString query, QStandardItemModel* model)
     {
-        std::vector<std::vector<std::string> > output;
-        std::vector<std::string> tmp;
+        std::cout<<"start exec"<<std::endl;
+        QList<QStandardItem*> tmp;
         nb_execute_sql(connection , query.toStdU16String().c_str(), size_t(query.size()));
         check_query(connection);
         int field_count = nb_field_count(connection);
@@ -149,9 +145,15 @@ private:
         {
             NBValue name;
             nb_field_name(connection, i, &name);
-            tmp.push_back(from_nbvalue(name).toStdString());
+            tmp.push_back(new QStandardItem(from_nbvalue(name)));
         }
-        output.push_back(tmp);
+
+        std::cout<<"write header"<<std::endl;
+
+        model->appendRow(tmp);
+        std::cout<<"write header"<<std::endl;
+
+
         int row_count = 0;
         while (nb_fetch_row(connection) == NB_OK)
         {
@@ -161,15 +163,14 @@ private:
             for (int column_i = 0; column_i<field_count; column_i+=1)
             {
                 nb_field_value_utf8(connection, column_i, &value);
-                tmp.push_back(from_nbvalue(value).toStdString());
+                tmp.push_back(new QStandardItem(from_nbvalue(value)));
             }
-            output.push_back(tmp);
+            model->appendRow(tmp);
         }
-        return output;
+        std::cout<<"end exec"<<std::endl;
     }
 
 private:
-    //QTableWidget* tableWidget_ = nullptr;
     QTableView* tableWidget_ = nullptr;
     QGridLayout* gridLayout_ = nullptr;
     QTextEdit* textEdit_ = nullptr;
@@ -177,6 +178,7 @@ private:
 
     std::vector<std::string> input_queries_;
     std::vector<std::vector<std::vector<std::string> > > responces_;
+    std::vector<QStandardItemModel*> models_;
     ResponceView* currentModel_ = nullptr;
 public:
     int dbPort_;
