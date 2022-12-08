64b9b42eb9048ce9af0063a4a3cc9c8e078fe46a
Ziabric <kurbet12312@gmail.com>
2022-12-08 22:34:30 +0300

    minor

--- syntaxHighlightingExample/mainwindow.cpp
+++ syntaxHighlightingExample/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QFile>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -11,5 +13,49 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->textEdit->append(paintText("sample teaxt with color", 0));
+    ui->textEdit->append(paintText("sample teaxt with color", 1));
+    ui->textEdit->append(paintText("sample teaxt with color", 2));
+
+    QFile file("output.txt");
+    file.open(QIODevice::ReadWrite);
+    if (file.isOpen())
+    {
+        file.write(ui->textEdit->toPlainText().toUtf8());
+    }
+    file.close();
+}
+
+
+void MainWindow::on_textEdit_textChanged()
+{
+//    QString word;
+//    QString str = ui->textEdit->toPlainText();
+//    int j;
+//    for (int i = 0;i<ui->textEdit->toPlainText().size();i+=1)
+//    {
+//        word = "";
+//        j = i;
+//        //for (int j = i;j<ui->textEdit->toPlainText().size();j+=1)
+//        while(str.at(j) != " " && j < ui->textEdit->toPlainText().size())
+//        {
+//            word+=str.at(j);
+//            j+=1;
+//        }
+//        auto iterNull = alp.find("-1");
+//        if (alp.find(word) != iterNull)
+//        {
+//            str.remove(i,j);
+//            str.insert(i, paintText(word, 0));
+//        }
+//        i = j;
+//    }
+    //надо найти подстроку в строке textEdit, удалить оригинал и вставить html код
 }
 
--- syntaxHighlightingExample/mainwindow.h
+++ syntaxHighlightingExample/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <map>
+#include <QString>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +17,35 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
+    QString paintText(QString str, int color)
+    {
+        QString output;
+        switch (color)
+        {
+            case 0 : output = QString("<span style=\" color:#ff0000;\">%1</span>").arg(str); break;
+            case 1 : output = QString("<span style=\" color:#008000;\">%1</span>").arg(str); break;
+            case 2 : output = QString("<span style=\" color:#0000FF;\">%1</span>").arg(str); break;
+        }
+        return output;
+    }
+
+
+    void on_textEdit_textChanged();
+
 private:
     Ui::MainWindow *ui;
+    std::map<QString, QString> alp =
+    {
+        {"select","blue"},
+        {"from","blue"},
+        {"top","blue"},
+        {"where","blue"},
+        {"create","blue"},
+        {"delete","blue"},
+        {"insert","blue"}
+    };
 };
 #endif // MAINWINDOW_H
