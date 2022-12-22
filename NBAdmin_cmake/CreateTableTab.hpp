7f6fcbb45d7f047c614f0bd1b532f40ab491e4da
ziabric <kurbet12312@gmail.com>
2022-12-22 07:58:43 -0800

    add combo box for types

--- NBAdmin_cmake/CreateTableTab.hpp
+++ NBAdmin_cmake/CreateTableTab.hpp
@@ -7,6 +7,7 @@
 #include <QLabel>
 #include <QPushButton>
 #include <QGridLayout>
+#include <QComboBox>
 #include <QStringList>
 
 #include "ui_CreateTableTab.h"
@@ -22,20 +23,67 @@ public:
     explicit CreateTableTab(QWidget* parent = nullptr) : QWidget(parent), ui(new Ui::CreateTableTab)
     {
         ui->setupUi(this);
+
+//        addRowButton_ = new QPushButton(this);
+//        rmRowButton_ = new QPushButton(this);
+
+
+
+        ui->tableWidget->setColumnCount(headerTable.count());
         ui->tableWidget->setHorizontalHeaderLabels(headerTable);
+
+        addRow();
+
+        connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(addRow()));
     }
     ~CreateTableTab()
     {
         delete ui;
     }
 
+
     void SetCurrentDatabase(QString& name)
     {
         ui->label->setText("Create Table in " + name);
     }
+
+public slots:
+    void addRow()
+    {
+        QComboBox* typesComboBox = new QComboBox();
+        QComboBox* FKTableComboBox = new QComboBox();
+
+        for (auto item : fieldsTypes_)
+        {
+            typesComboBox->addItem(item);
+        }
+
+
+
+        ui->tableWidget->insertRow(ui->tableWidget->rowCount());
+        ui->tableWidget->setCellWidget(ui->tableWidget->rowCount() - 1, 1, typesComboBox);
+        ui->tableWidget->setCellWidget(ui->tableWidget->rowCount() - 1, 4, FKTableComboBox);
+    }
+
 private:
     QStringList headerTable = {"Name", "Type", "PK", "FK", "FK table", "Identity", "NOT NULL" , ""};
     Ui::CreateTableTab* ui;
 public:
     QString currentDatabase_ = "";
+    QPushButton* addRowButton_ = nullptr;
+    QPushButton* rmRowButton_ = nullptr;
+
+    std::vector<QString> fieldsTypes_ = {
+        "varchar",
+        "int",
+        "bigint",
+        "double",
+        "datetime",
+        "bit",
+        "date",
+        "varbinary",
+        "nvarchar",
+        "rowversion",
+        "decimal"
+    };
 };
