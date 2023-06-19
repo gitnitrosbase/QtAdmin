2e678e87ba92a255377c38187996eb824e7e291b
ziabric <kurbet12312@gmail.com>
2023-06-19 10:31:10 -0700

    fixed 'create table'

--- NBAdmin/MainWindow.cpp
+++ NBAdmin/MainWindow.cpp
@@ -350,48 +350,55 @@ void MainWindow::on_actionCreateDBQueryTrig()
         QJsonDocument doc(obj);
         QByteArray data = doc.toJson();
         QNetworkReply *reply = mgr->post(request, data);
-        connect(reply, &QNetworkReply::finished, [=]()
+
+        QEventLoop loop;
+        QObject::connect(reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+        loop.exec();
+
+        QString finalQuery = QString("CREATE TABLE %1 (").arg(ui->treeWidget->currentItem()->text(0));
+        if(reply->error() == QNetworkReply::NoError)
         {
-            QString finalQuery = QString("CREATE TABLE %1 (").arg(ui->treeWidget->currentItem()->text(0));
-            if(reply->error() == QNetworkReply::NoError)
+            QString strReply = reply->readAll();
+            QJsonArray tables = QJsonDocument::fromJson(strReply.toUtf8()).object().find("data")->toArray();
+
+            for (auto item : tables)
             {
-                QString strReply = reply->readAll();
-                QJsonArray tables = QJsonDocument::fromJson(strReply.toUtf8()).object().find("data")->toArray();
-
-                for (auto item : tables)
+                if (ui->treeWidget->currentItem()->text(0) == item.toObject().find("tablename")->toString())
                 {
-                    if (ui->treeWidget->currentItem()->text(0) == item.toObject().find("tablename")->toString())
-                    {
 
-                        for (auto fields : item.toObject().find("fields")->toArray())
-                        {
-                            QString query = "\n\t";
-                            query += QString(fields.toObject().find("name")->toString() + " ");
-                            query += QString(getType(fields.toObject()) + " ").toUpper();
-                            if(fields.toObject().find("subtype")->toInt() == 1)
-                                query += "PRIMARY KEY NOT NULL";
-                            if (fields.toObject().find("linktable")->toString() != "")
-                                query += QString("FOREIGN KEY(%1) REFERENCES %2").arg(ui->treeWidget->currentItem()->text(0)).arg(fields.toObject().find("linktable")->toString());
-                            if (fields.toObject().find("seed")->toInt() != 0 && fields.toObject().find("increment")->toInt() != 0)
-                                query += QString("IDENTITY (%1,%2)").arg(fields.toObject().find("seed")->toInt()).arg(fields.toObject().find("increment")->toInt());
-                            if (fields.toObject().find("nullable")->toInt() == 0 && fields.toObject().find("subtype")->toInt() != 1)
-                                query += "NOT NULL";
-                            if (!fields.toObject().find("defaultvalue")->isNull())
-                                query += QString(" DEFAULT '%1'").arg(fields.toObject().find("defaultvalue")->toString());
-                            query += ",";
-                            finalQuery+=query;
-                        }
-                        finalQuery.resize(finalQuery.size()-1);
-                        finalQuery+="\n);";
+                    for (auto fields : item.toObject().find("fields")->toArray())
+                    {
+                        QString query = "\n\t";
+                        query += QString(fields.toObject().find("name")->toString() + " ");
+
+                        if (fields.toObject().find("seed")->toInt() != 0 && fields.toObject().find("increment")->toInt() != 0)
+                            query += QString("IDENTITY (%1,%2)").arg(fields.toObject().find("seed")->toInt()).arg(fields.toObject().find("increment")->toInt());
+                        else query += QString(getType(fields.toObject())).toUpper();
+
+                        if(fields.toObject().find("subtype")->toInt() == 1)
+                            query += " PRIMARY KEY NOT NULL";
+
+                        if (fields.toObject().find("linktable")->toString() != "")
+                            query += QString(" FOREIGN KEY(%1) REFERENCES %2").arg(ui->treeWidget->currentItem()->text(0)).arg(fields.toObject().find("linktable")->toString());
+
+                        if (fields.toObject().find("nullable")->toInt() == 0 && fields.toObject().find("subtype")->toInt() != 1)
+                            query += " NOT NULL";
+
+                        if (!fields.toObject().find("defaultvalue")->isNull())
+                            query += QString(" DEFAULT '%1'").arg(fields.toObject().find("defaultvalue")->toString());
+                        query += ",";
+                        finalQuery+=query;
                     }
+                    finalQuery.resize(finalQuery.size()-1);
+                    finalQuery+="\n);";
                 }
-                push_button_plus_clicked();
-                ui->tabWidget->setCurrentIndex(ui->tabWidget->count()-1);
-                TabWindow* currentTab = dynamic_cast<TabWindow*>(ui->tabWidget->currentWidget());
-                currentTab->textEdit_->setText(finalQuery);
             }
-            reply->deleteLater();
-        });
+            push_button_plus_clicked();
+            ui->tabWidget->setCurrentIndex(ui->tabWidget->count()-1);
+            TabWindow* currentTab = dynamic_cast<TabWindow*>(ui->tabWidget->currentWidget());
+            currentTab->textEdit_->setText(finalQuery);
+        }
+        delete reply;
     }
 }
 void MainWindow::on_actionSelectEdgeTrig()
